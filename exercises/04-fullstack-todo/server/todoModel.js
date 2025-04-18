46c12f68097e3f7c7a39bd10c5a8f344247292db
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2025-04-18 17:54:26 +0000

    update

--- exercises/04-fullstack-todo/server/todoModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const todoSchema = new mongoose.Schema({
-  text: String,
-  completed: { type: Boolean, default: false }
-})
-
-export default mongoose.model('Todo', todoSchema)
