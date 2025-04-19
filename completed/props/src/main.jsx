dd358d94bb458d3480788179765f2c2d9886fafb
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2025-04-19 08:12:14 +0000

    update.

--- completed/props/src/main.jsx
+++ completed/props/src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client"
+import React from "react"
+
 const root = createRoot(document.getElementById("root"))
 
 function TodoItem(props) {
-  return(
+  return (
     <li>
       <h3>{props.title}</h3>
       <p>Status: {props.status}</p>
@@ -11,17 +13,21 @@ function TodoItem(props) {
 }
 
 function Page() {
+  const todos = [
+    { title: "Full Stack Development", status: "In Progress" },
+    { title: "Mobile App Development", status: "To Do" }
+  ]
+
   return (
     <div>
       <h1>Fullstack To-Do App</h1>
       <ul>
-        <TodoItem title="Full Stack Development" status="In Progress" />
-        <TodoItem title="Mobile App Development" status="To Do" />
+        {todos.map((todo, index) => (
+          <TodoItem key={index} title={todo.title} status={todo.status} />
+        ))}
       </ul>
     </div>
   )
 }
 
-root.render(
-  <Page />
-)
+root.render(<Page />)
