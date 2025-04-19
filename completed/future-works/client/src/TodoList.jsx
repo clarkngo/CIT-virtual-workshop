604082ebb5c2633d06fe27a52d155666b5ee0c49
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2025-04-19 07:13:24 +0000

    update

---
+++ completed/future-works/client/src/TodoList.jsx
@@ -0,0 +1,24 @@
+function TodoList({ todos, toggleTodo, deleteTodo }) {
+    return (
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+                marginRight: '1rem',
+                cursor: 'pointer'
+              }}
+              onClick={() => toggleTodo(todo._id)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo._id)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  
+  export default TodoList
+  
